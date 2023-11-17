c802b43aeb18bcbe4694e2f2662f58fb6b56810e
Rickie Blaese <rkyleblaese@gmail.com>
2023-11-16 21:51:29 -0800

    fix compilesh

--- compile.sh
+++ compile.sh
@@ -1,1 +1,1 @@
-clang++-16 -std=c++20 -DUSE_MULTIMEDIA=none -o rayt src/common.cc src/models.cc src/rayt.cc $(pkg-config --cflags --libs notcurses) -g -O3 -Wall # -fsanitize=address
+clang++ -std=c++20 -DUSE_MULTIMEDIA=none -o rayt src/common.cc src/models.cc src/rayt.cc $(pkg-config --cflags --libs notcurses) -g -O3 -Wall # -fsanitize=address
